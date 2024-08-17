3cf442432e480824db61621cd2071398fae2bb2d
Zachary Lasley <116044492+Zlasley@users.noreply.github.com>
2024-08-17 12:28:46 -0500

    Update looney.py

--- looney.py
+++ looney.py
@@ -5,7 +5,7 @@ class Product:
     """Class attributes"""
 
     def __init__(self, name=None, price=10, weight=20, flammability=0.5):
-        
+        self.name = name
         self.price = price
         self.weight = weight
         self.flammability = flammability
